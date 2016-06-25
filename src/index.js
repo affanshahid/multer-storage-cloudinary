7fff423452b0d784d2a1f6669cad437f172d1f4a
Affan Shahid <affan.shahid.94@gmail.com>
2016-06-25 17:24:58 +0500

    git add more opts, provision to directly supply cloudinary params

--- src/index.js
+++ src/index.js
@@ -5,15 +5,35 @@ class CloudinaryStorage {
     if (!opts || !opts.cloudinary)
       throw new Error('`cloudinary` option required');
     this.cloudinary = opts.cloudinary;
-    this.getFilename = opts.filename || this._noop;
 
-    if (typeof opts.folder === 'string') {
-      this.getFolder = this._staticVal(opts.folder);
-    } else if (typeof opts.folder === 'function') {
-      this.getFolder = opts.folder;
+    this.getFilename = this._getParamGetter('filename', undefined, opts);
+    this.getFolder = this._getParamGetter('folder', 'string', opts);
+    this.getTransformation = this._getParamGetter('transformation',
+      'string', opts);
+    this.getType = this._getParamGetter('type', 'string', opts);
+    this.getFormat = this._getParamGetter('format', 'string', opts);
+    this.getParams = this._getParamGetter('params', 'object', opts);
+  }
+
+  _getParamGetter(name, type, opts) {
+    let param;
+    if (typeof opts[name] === 'function') {
+      param = opts.transformation;
+    } else if (type && typeof opts[name] === type) {
+      param = this._staticVal(opts.transformation);
+    } else if (opts[name] == undefined) {
+      param = this._staticVal(undefined);
     } else {
-      this.getFolder = this._noop;
+      let errTemp;
+      if (type) {
+        errTemp = `, 'undefined' or '${type}'`;
+      } else {
+        errTemp = ` or 'undefined'`;
+      }
+      throw new TypeError(`Expected opts.${name} to be of types 'function'` +
+        errTemp);
     }
+    return param;
   }
 
   _handleFile(req, file, cb) {
@@ -21,13 +41,22 @@ class CloudinaryStorage {
     runParallel(
       [
         this.getFolder,
-        this.getFilename
+        this.getFilename,
+        this.getTransformation,
+        this.getType,
+        this.getFormat,
+        this.getParams
       ],
       (err, results) => {
-        const stream = this.cloudinary.v2.uploader.upload_stream({
+        const params = results[5] || {
           folder: results[0],
-          public_id: results[1]
-        }, cb);
+          public_id: results[1],
+          transformation: results[2],
+          type: results[3],
+          format: results[4]
+        };
+        
+        const stream = this.cloudinary.v2.uploader.upload_stream(params, cb);
 
         file.stream.pipe(stream);
       });
@@ -37,13 +66,9 @@ class CloudinaryStorage {
     this.cloudinary.v2.uploader.destroy(file.file_id, { invalidate: true }, cb);
   }
 
-  _noop(_, __, cb) {
-    cb();
-  }
-
   _staticVal(val) {
     return function (_, __, cb) {
-      cb(null, val);
+      cb(undefined, val);
     };
   }
 }
